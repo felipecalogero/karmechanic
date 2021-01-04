95fc245f04da5de8e44d2058a684e9ff89e16b62
Felipe Augusto Calógero <felipea1994@gmail.com>
2021-01-04 11:59:43 -0300

    initial page changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,8 +3,10 @@ $(function () {
         $(window).on('scroll', function() {
             if($(window).scrollTop() > 10 ) {
                 $('.navbar').addClass('active');
+                $('.navbar-brand').removeClass('logo-none');
             } else {
                 $('.navbar').removeClass('active');
+                $('.navbar-brand').addClass('logo-none');
             }
         });
 
