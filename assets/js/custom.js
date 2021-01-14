cc5fb4db7962a1bd2c45d7159c653b269337c728
Felipe Calógero <felipea1994@gmail.com>
2021-01-14 20:11:32 -0300

    settings and content alignment

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,32 @@
 $(function () {
 
-        $(window).on('scroll', function() {
-            if($(window).scrollTop() > 10 ) {
-                $('.navbar').addClass('active');
-                $('.navbar-brand').removeClass('logo-none');
-            } else {
-                $('.navbar').removeClass('active');
-                $('.navbar-brand').addClass('logo-none');
-            }
-        });
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 10) {
+            $('.navbar').addClass('active');
+            $('.navbar-brand').removeClass('logo-none');
+        } else {
+            $('.navbar').removeClass('active');
+            $('.navbar-brand').addClass('logo-none');
+        }
+    });
+});
 
+function ativaNoScroll() {
+    document.getElementById('services').querySelectorAll('img').forEach((img, index) => {
+        if (img.getBoundingClientRect().top < window.innerHeight) {
+            img.src = img.getAttribute('data-src');
+        };
+    })
+}
+
+window.addEventListener('scroll', ativaNoScroll);
+
+$('.navbar-nav a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
 });
---
+++ index.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Kar Mechanic</title>
+    <link rel="shortcut icon" href="assets/img/favicon.ico" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.3/tiny-slider.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+            <div class="container">
+                <a class="navbar-brand logo-none" href="#"><img src="assets/img/logo-nav.png" loading="lazy" alt="Apper"></a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <!-- <label for="rd_start" class="nav-link">Inicio</label> -->
+                            <a href="#start" class="nav-link">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <!-- <label for="rd_about" class="nav-link">Sobre</label> -->
+                            <a href="#about" class="nav-link">Sobre</a>
+                        </li>
+                        <li class="nav-item">
+                            <!-- <label for="rd_services" class="nav-link">Serviços</label> -->
+                            <a href="#services" class="nav-link">Serviços</a>
+                        </li>
+                        <li class="nav-item">
+                            <!-- <label for="rd_photoscars" class="nav-link">Fotos</label> -->
+                            <a href="#photoscars" class="nav-link">Fotos</a>
+                        </li>
+                        <li class="nav-item">
+                            <!-- <label for="rd_contact" class="nav-link">Contato</label> -->
+                            <a href="#contact" class="nav-link">Contato</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <!-- Hero section -->
+
+    <section id="start" class="sections">
+        <div class="container">
+            <div class="row main-hero-content">
+                <div class="col-md-6">
+                    <h1>MAKE YOUR LIFE EASIER WITH APPER</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non architecto facere asperiores
+                        praesentium cumque voluptatum, quod labore quas quo rem quos deserunt earum ipsum inventore,
+                        doloremque maiores? Recusandae, mollitia quis!</p>
+                    <div class="hero-buttons">
+                        <a href="#about" class="btn btn-outline-primary btn-white">conheça</a>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="image-box">
+                        <img src="assets/img/logo.png" class="img-fluid" loading="lazy" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- About section -->
+
+    <section id="about" class="sections">
+        <div class="container">
+            <div class="row section-title justify-content-center">
+                <h2 class="section-title-heading">Sobre</h2>
+            </div>
+            <div class="row justify-content-center text-center mt-5">
+                A <strong>&nbsp;KAR MECHANIC&nbsp;</strong> foi criada com o intuito de se tornar referência no mercado
+                em sua área de atuação.
+            </div>
+            <div class="row justify-content-center text-center mt-5">
+                <div class="col-md-4">
+                    <i class="fas fa-cogs fa-3x mb-2"></i>
+                    <h3>Qualidade</h3>
+                    <p>Trabalhamos com equipe de profissionais eficientes e comprometidos em superar as expectativas de
+                        nossos clientes.</p>
+                </div>
+                <div class="col-md-4">
+                    <i class="fas fa-bell fa-3x mb-2"></i>
+                    <h3>Transparência</h3>
+                    <p>Atuamos com seriedade, transparência e, sobretudo, respeito, prezando sempre pela qualidade na
+                        execução dos serviços.</p>
+                </div>
+                <div class="col-md-4">
+                    <i class="fas fa-power-off fa-3x mb-2"></i>
+                    <h3>Satisfação</h3>
+                    <p>Estamos sempre prontos para atender e satisfazer as necessidades dos nossos clientes.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Services section -->
+
+    <section id="services" class="sections">
+        <div class="container">
+            <div class="row no-gutters section-title justify-content-center">
+                <h2 class="section-title-heading">Serviços</h2>
+            </div>
+            <div class="row no-gutters mt-5">
+                <div class="service-block col-sm">
+                    <img data-src="assets/img/embreagem.png" loading="lazy" alt="">
+                </div>
+                <div class="service-block col-sm">
+                    <img data-src="assets/img/revisao.png" loading="lazy" alt="">
+                </div>
+            </div>
+            <div class="row no-gutters mt-5">
+                <div class="service-block col-sm">
+                    <img data-src="assets/img/freios-esq.png" loading="lazy" alt="">
+                </div>
+                <div class="service-block col-sm">
+                    <img data-src="assets/img/arrefecimento-dir.png" loading="lazy" alt="">
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="service-block col-sm">
+                    <img data-src="assets/img/bicos-esq.png" loading="lazy" alt="">
+                </div>
+                <div class="service-block col-sm">
+                    <img data-src="assets/img/eletrico-dir.png" loading="lazy" alt="">
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="service-block col-sm">
+                    <img data-src="assets/img/injecao-esq.png" loading="lazy" alt="">
+                </div>
+                <div class="service-block col-sm">
+                    <img data-src="assets/img/suspensao-dir.png" loading="lazy" alt="">
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="service-block col-sm">
+                    <img data-src="assets/img/motores-esq-ult.png" loading="lazy" alt="">
+                </div>
+                <div class="service-block col-sm">
+                    <img data-src="assets/img/oleo-dir-ult.png" loading="lazy" alt="">
+                </div>
+            </div>
+        </div>
+        </div>
+    </section>
+
+    <!-- Photoscars section -->
+
+    <section id="photoscars" class="sections">
+        <div class="container">
+            <div class="row photoscars-title justify-content-center">
+                <h2>Serviços realizados</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae illum asperiores deleniti. Facere,
+                    possimus
+                    architecto necessitatibus alias veritatis aperiam eius quibusdam vel ducimus fuga
+                    delectus, velit, temporibus consequatur voluptatibus a.</p>
+
+                <!-- Photos -->
+
+                <?php
+                $listaImagens = [
+                    "/assets/img/cars/car1.jpg",
+                    "/assets/img/cars/car2.jpg",
+                    "/assets/img/cars/car3.jpg",
+                    "/assets/img/cars/car4.jpg",
+                    "/assets/img/cars/car5.jpg",
+                    "/assets/img/cars/car6.jpg",
+                    "/assets/img/cars/car7.jpg",
+                    "/assets/img/cars/car8.jpg",
+                    "/assets/img/cars/car9.jpg",
+                    "/assets/img/cars/car10.jpg",
+                    "/assets/img/cars/car11.jpg",
+                    "/assets/img/cars/car12.jpg",
+                    "/assets/img/cars/car13.jpg"
+                ];
+
+                foreach ($listaImagens as $key => $imagem) {
+                ?>
+
+                    <div class="row zoom-gallery miniaturas justify-content-center">
+                        <div id="car<?= $key ?>">
+                            <a href="" onclick="abrirJanela(<?= $imagem ?>)"><img src="<?= $imagem ?>"></a>
+                        </div>
+                    </div>
+
+                <?php
+                }
+                ?>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact section -->
+
+    <section id="contact" class="sections">
+        <div class="container">
+            <div class="row section-title justify-content-center">
+                <h2 class="section-title-heading">Contato</h2>
+            </div>
+            <div class="row mt-5">
+                <div class="col-md-8">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3698.4875028829715!2d-47.88889068546583!3d-22.030939585464143!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94b8771c64843eef%3A0xc38f7494d91b8356!2sR.%20Vicente%20de%20Carvalho%2C%20426%20-%20Vila%20Marcelino%2C%20S%C3%A3o%20Carlos%20-%20SP%2C%2013570-593!5e0!3m2!1spt-BR!2sbr!4v1610656244867!5m2!1spt-BR!2sbr" 
+                     width="600" height="450" frameborder="0" style="border: solid #2B323F 2px; border-radius: 10px" allowfullscreen="" aria-hidden="false" tabindex="0">
+                </iframe>
+                </div>
+                <div class="col-md-4">
+                    <div class="address">
+                        <h3>Endereço:</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde voluptatibus quae ea nihil
+                            tempora nemo voluptatum praesentium soluta nam?</p>
+                    </div>
+                    <div class="contact-details">
+                        <ul class="fa-ul">
+                            <li><span class="fa-li"><i class="fas fa-mobile-alt"></i></span>(16) 3413-1018</li>
+                            <li><span class="fa-li"><i class="fas fa-map-marker-alt"></i></span>R. Vicente de Carvalho, 426 - Vila Marcelino, São Carlos - SP, 13570-593</li>
+                            <li><span class="fa-li"><i class="fas fa-paper-plane"></i></span>karofmechanic@gmail.com</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer section -->
+
+    <footer id="footer">
+        <div class="container">
+            <div class="row d-flex flex-column align-items-center">
+                <div class="footer-logo">
+                    <img src="assets/img/logo.png" alt="">
+                </div>
+                <div class="social-icons">
+                    <a href="https://www.facebook.com/kar.mechanic" target="_blank"><i class="fab fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/karofmechanic/" target="_blank"><i class="fab fa-instagram"></i></a>
+                </div>
+                <div class="copyright text-center">
+                    <p>Copyright <?= date("Y") ?></p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="assets/js/custom.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.2/min/tiny-slider.js"></script>
+</body>
+</html>
